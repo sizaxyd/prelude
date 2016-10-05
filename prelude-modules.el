b96d668bbc72071869b53886b1070e0df26925c0
harm <sizaxyd@gmail.com>
2016-10-05 17:05:19 +0500

    moved prelude-modules

--- prelude-modules.el
+++ prelude-modules.el
@@ -1,10 +1,9 @@
 ;;; Uncomment the modules you'd like to use and restart Prelude afterwards
 
-;; Emacs IRC client
-(require 'prelude-erc)
+(require 'prelude-erc) ;; Emacs IRC client
 (require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
-;; (require 'prelude-helm) ;; Interface for narrowing and search
-;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
+(require 'prelude-helm) ;; Interface for narrowing and search
+(require 'prelude-helm-everywhere) ;; Enable Helm everywhere
 (require 'prelude-company)
 ;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
 ;; (require 'prelude-mediawiki)
@@ -27,7 +26,7 @@
 ;; (require 'prelude-ocaml)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
-;; (require 'prelude-python)
+(require 'prelude-python)
 ;; (require 'prelude-ruby)
 ;; (require 'prelude-scala)
 (require 'prelude-scheme)
